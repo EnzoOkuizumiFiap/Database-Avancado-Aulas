e184ca68f73ac2d6241983ecd0fd18d662cb4f2f
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2026-08-13 13:17:23 -0300

    Aula 10 - Triggers e Packages

---
+++ Aula 10 - Triggers e Packages/packages.sql
@@ -0,0 +1,50 @@
+-- é tipo packages em Java... Podemos ter na package várias procedures também e selecionar quais queremos "exportar"
+--create table insert_clientes as select * from pf1788.insert_clientes;
+
+
+
+
+create or replace PACKAGE pkg_aula_01 as 
+    PROCEDURE insert_cliente (
+        p_id number,
+        p_nome varchar2,
+        p_email varchar2,
+        p_data_cadastro date
+    );
+    
+    function contar_clientes return number;
+    
+end pkg_aula_01;
+
+
+
+
+create or replace PACKAGE BODY pkg_aula_01 AS
+
+    PROCEDURE insert_cliente (
+        p_id            NUMBER,
+        p_nome          VARCHAR2,
+        p_email         VARCHAR2,
+        p_data_cadastro DATE
+    ) IS
+    BEGIN
+        INSERT INTO insert_clientes VALUES ( p_id, p_nome, p_email, p_data_cadastro);
+        COMMIT;
+    END insert_cliente;
+
+    FUNCTION contar_clientes RETURN NUMBER IS v_total NUMBER;
+    BEGIN 
+        SELECT COUNT(1) INTO v_total FROM insert_clientes;
+        RETURN v_total;
+    END contar_clientes;
+
+END pkg_aula_01;
+
+
+
+
+
+-- Como utilizar uma package
+select * from insert_clientes;
+
+CALL pkg_aula_01.insert_cliente(10, 'João', 'dasdas@hotmail', sysdate);
